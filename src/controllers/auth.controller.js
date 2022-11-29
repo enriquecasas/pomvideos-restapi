512c84ee1269a8c1f60ace0a571aa87c45fd40f1
enriquecasas <cenriquecasas@gmail.com>
2022-11-29 18:42:22 -0500

    Implementación de jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,17 +4,19 @@ import jwt from "jsonwebtoken"
 export const authUser = async (req, res) => {
     try {
         const { usuario, clave } = req.body
-        const [rows] = await pool.query('SELECT * FROM b9dw0vobstpr7etg0eho.user where userName=? and pass=?',[usuario,clave])
+        const [rows] = await pool.query('SELECT userName, roleId FROM b9dw0vobstpr7etg0eho.user where userName=? and pass=?',[usuario,clave])
 
-        if (rows.length <= 0) return res.status(404).json({
-            message: 'User not found'
-        })
-
-        let data = JSON.stringify(rows[0]);
-        console.log(data)
-        const token = jwt.sign(data, process.env.KEY)
-        res.json({token})
-
+        if (rows.length > 0) {
+            let data = JSON.stringify(rows[0]);
+            console.log(data)
+            const token = jwt.sign(data, process.env.KEY)
+            res.json({token})
+        }else{
+            res.json('Usuario o clave incorrecta')
+            // return res.status(404).json({
+            //     message: 'User not found'
+            // })
+        }
     } catch (error) {
         return res.status(500).json({
             message: 'Something goes wrong ' + error
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
 import { authUser } from '../controllers/auth.controller.js'
+import jwt from "jsonwebtoken"
 
 const router = Router()
 
 router.post('/singin', authUser)
+
+router.post('/test', verifayToken, (req, res) => {
+    res.json('Informacion Secreta')
+})
+
+function verifayToken(req, res, next) {
+    if (!req.headers.authorization) return res.status(401).json('No autorizado');
+
+    const token = req.headers.authorization.substr(7);
+    if (token != '') {
+        const content = jwt.verify(token, process.env.KEY)
+        req.data = content;
+        next()
+    }else{
+        res.status(401).json('Token invalido')
+    }
+}
 
 export default router
